1b783c59918d8865e261e8460d2a3e4dbc2f526c
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-03-30 13:27:24 +0800

    Internationalization create two languages en and zh-CN yml

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = "en"
+    config.i18n.available_locales = [:en, 'zh-CN']
 
     # Don't generate system test files.
     config.generators.system_tests = nil
